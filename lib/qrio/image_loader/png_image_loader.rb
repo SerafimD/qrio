2437487a19f2fd9d72aaaacd329f72fa227d9706
Dmitry Sinelnikov <dmitry.sinelnikov@project30.pro>
2017-08-07 19:18:00 +0300

    fix

--- lib/qrio/image_loader/png_image_loader.rb
+++ lib/qrio/image_loader/png_image_loader.rb
@@ -7,7 +7,7 @@ module Qrio
         bits = image.pixels.map do |pixel|
           grayscale = ChunkyPNG::Color.to_grayscale(pixel)
           level = ChunkyPNG::Color.r(grayscale)
-          level <= 512
+          level <= 126
         end
 
         Matrix.new(bits, image.width, image.height)
